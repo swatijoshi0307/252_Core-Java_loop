5d56528380a4882fae9522f53500aee213f6df5e
swatijoshi0307 <swatijoshi0307@gmail.com>
2024-09-17 09:52:16 -0400

    First commit

---
+++ src/corejavaloop/Test.java
@@ -0,0 +1,13 @@
+package corejavaloop;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		  for(int i=1;i<=10;i++) {
+			  System.out.println(i);
+		  }
+	}
+
+}
